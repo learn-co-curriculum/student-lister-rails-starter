eb7113f92394810ffbbb1fd9a82e0a4b47e9d2d0
laurkim <laurakeem@gmail.com>
2018-02-27 09:47:11 -0500

    create see data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 require 'date'
 
-Student.create(name: 'Meryl', email: 'meryl@meryl.com', dob: Date.parse('2017-11-16'))
-Student.create(name: 'Alex', email: 'alex@alex.com', dob: Date.parse('2017-07-02'))
-Student.create(name: 'Alex', email: 'alex@alex.com', dob: Date.parse('2017-07-02'))
+Student.create(name: 'Niky', email: 'niky@niky.com', dob: Date.parse('2018-01-29'))
+Student.create(name: 'Laura', email: 'laura@laura.com', dob: Date.parse('2018-01-02'))
+Student.create(name: 'Laura', email: 'laura@laura.com', dob: Date.parse('2018-01-02'))
 Student.create(name: '')
-Student.create(name: 'asdf')
-Student.create(name: 'Lindsey', email: 'lindsey@lindsey.com', dob: Date.parse('2017-11-16'))
+Student.create(name: 'notarealname')
+Student.create(name: 'Matt', email: 'Matt@matt.com', dob: Date.parse('2018-01-29'))
