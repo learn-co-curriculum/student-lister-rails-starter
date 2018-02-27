8614cb1db35f6e277ea07643819d890431fe693d
laurkim <laurakeem@gmail.com>
2018-02-27 09:48:02 -0500

    create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,6 @@ require 'date'
 Student.create(name: 'Niky', email: 'niky@niky.com', dob: Date.parse('2018-01-29'))
 Student.create(name: 'Laura', email: 'laura@laura.com', dob: Date.parse('2018-01-02'))
 Student.create(name: 'Laura', email: 'laura@laura.com', dob: Date.parse('2018-01-02'))
+Student.create(name: 'Matt', email: 'Matt@matt.com', dob: Date.parse('2018-01-29'))
 Student.create(name: '')
 Student.create(name: 'notarealname')
-Student.create(name: 'Matt', email: 'Matt@matt.com', dob: Date.parse('2018-01-29'))
