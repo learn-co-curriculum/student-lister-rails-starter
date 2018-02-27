0d7362b9eec2c1255467161f7baa3f4755f180e5
laurkim <laurakeem@gmail.com>
2018-02-27 15:24:34 -0500

    mid lecture

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,13 @@
 class Student < ApplicationRecord
+  validates :name, :email, presence: true
+  validate :student_over_18
+  # 1. email be unique
+  # 2. each student be over 18
+    # dob >= 18.years.ago
+  # 3. no student can be named "Matt Lawford"
+
+  def student_over_18
+    # checks whether or not the date of birth is more than 18 years ago
+    # self
+  end
 end
