5f30044e7aab87957f637fbf1f6feeaf50211571
laurkim <laurakeem@gmail.com>
2018-02-27 14:55:21 -0500

    seed data for lecture

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@ Student.create(name: 'Laura', email: 'laura@laura.com', dob: Date.parse('2018-01
 Student.create(name: 'Laura', email: 'laura@laura.com', dob: Date.parse('2018-01-02'))
 Student.create(name: 'Matt', email: 'Matt@matt.com', dob: Date.parse('2018-01-29'))
 Student.create(name: '')
-Student.create(name: 'notarealname')
+Student.create(name: 'notaREALname')
